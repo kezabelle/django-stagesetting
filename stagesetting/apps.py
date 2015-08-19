6108cc7dc65ed8d35e691c5d6a69a08a4eb50414
Keryn Knight <keryn@kerynknight.com>
2015-08-19 19:50:58 +0100

    This exception handling is no longer necessary since removing the creation of values on startup in 85ce0147c0f56b2780f088a5617738884821ecdd

--- stagesetting/apps.py
+++ stagesetting/apps.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 import logging
 from django.core.checks import registry as django_check_registry
-from django.db.utils import OperationalError
 from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
 
@@ -20,13 +19,5 @@ class StageSettingAppConfig(AppConfig):
         from .checks import check_setting
         from .utils import registry as stagesetting_registry
         django_check_registry.register(check_setting)
-        # So I want to patch the defaults in by default, but apparently I
-        # can't do so without migrating, nor in post_migrate.
-        # I don't yet know in what ways this will break.
-        try:
-            stagesetting_registry.ready(sender=self.__class__, instance=self,
+        stagesetting_registry.ready(sender=self.__class__, instance=self,
                                         model=RuntimeSetting)
-        except OperationalError as exc:
-            logger.warning("Could not do the database work in AppConfig.ready, "
-                           "probably because nothing has migrated yet",
-                           exc_info=1)
