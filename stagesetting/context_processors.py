f7f893bea5edec04cfea59f57c527c2f0fea8b5d
Keryn Knight <keryn@kerynknight.com>
2016-01-06 21:40:22 +0000

    Refs #6 - Allow replacing the context processor easily, though it sometimes assumes the middleware has also been appropriately replaced.

--- stagesetting/context_processors.py
+++ stagesetting/context_processors.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from .models import RuntimeSettingWrapper
+from .models import RuntimeSettingWrapper, RuntimeSetting
 
 
-def runtime_settings(request):
+def runtime_settings_for_model(request, model):
     if hasattr(request, 'stagesetting'):
         settings = request.stagesetting
     else:
-        settings = RuntimeSettingWrapper()
+        settings = RuntimeSettingWrapper(model=model)
     return {
         'STAGESETTING': settings,
     }
+
+
+def runtime_settings(request):
+    return runtime_settings_for_model(request=request, model=RuntimeSetting)
