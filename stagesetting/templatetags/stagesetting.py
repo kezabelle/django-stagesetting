4d5886f6ab06352b04e5f8afc0e1016e0bda8e11
Keryn Knight <keryn@kerynknight.com>
2017-05-17 12:41:13 +0100

    Fully cover the possible paths for yielding the wrapper object in a template tag.

--- stagesetting/templatetags/stagesetting.py
+++ stagesetting/templatetags/stagesetting.py
@@ -4,8 +4,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 from django.template import Library
-from stagesetting.models import RuntimeSettingWrapper
-
+from stagesetting.models import RuntimeSettingWrapper, RuntimeSetting
 
 register = Library()
 
@@ -16,6 +15,8 @@ def stagesetting(context):
         wrapper = context['STAGESETTING']
     elif 'request' in context and hasattr(context['request'], 'stagesetting'):
         wrapper = context['request'].stagesetting
+    elif hasattr(context, 'request') and hasattr(context.request, 'stagesetting'):
+        wrapper = context.request.stagesetting
     else:
-        wrapper = RuntimeSettingWrapper()
+        wrapper = RuntimeSettingWrapper(model=RuntimeSetting)
     return wrapper
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from django.template import Template, Context
+from django.template import Template, Context, RequestContext
 import pytest
+
+from stagesetting.context_processors import runtime_settings
+from stagesetting.middleware import ApplyRuntimeSettings
+
+
+@pytest.fixture
+def stagesetting_mw():
+    return ApplyRuntimeSettings().process_view
 
 
 def test_loading():
@@ -12,3 +20,27 @@ def test_loading():
 @pytest.mark.django_db
 def test_works():
     assert Template("{% load stagesetting %}{% stagesetting as LOL %}{{ LOL|length }}").render(Context()) == '2'
+
+
+@pytest.mark.django_db
+def test_reuses_existing_context_variable_if_set_via_context_processor(rf):
+    request = rf.get('/')
+    context = Context()
+    context.update(runtime_settings(request=request))
+    assert Template("{% load stagesetting %}{% stagesetting as LOL %}{{ LOL|length }}").render(context) == '2'
+
+
+@pytest.mark.django_db
+def test_reuses_existing_context_variable_if_set_via_middleware_and_request_in_context(rf, stagesetting_mw):
+    request = rf.get('/')
+    stagesetting_mw(request=request, view_func=None, view_args=None, view_kwargs=None)
+    context = Context({'request': request})
+    assert Template("{% load stagesetting %}{% stagesetting as LOL %}{{ LOL|length }}").render(context) == '2'
+
+
+@pytest.mark.django_db
+def test_reuses_existing_context_variable_if_set_via_middleware_is_requestcontext(rf, stagesetting_mw):
+    request = rf.get('/')
+    stagesetting_mw(request=request, view_func=None, view_args=None, view_kwargs=None)
+    context = RequestContext(request, {})
+    assert Template("{% load stagesetting %}{% stagesetting as LOL %}{{ LOL|length }}").render(context) == '2'
