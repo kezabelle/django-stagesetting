a528b931d7b8c437757347fcf808c5f4c6fd41b3
Keryn Knight <keryn@kerynknight.com>
2017-05-22 16:27:02 +0100

    Refs #8 - first pass at getting a single configuration item, both in list style, and ostensibly as a dict (but not currently passed through json, maybe it should be?)

--- stagesetting/management/commands/stagesetting.py
+++ stagesetting/management/commands/stagesetting.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from stagesetting.models import RuntimeSetting, RuntimeSettingWrapper
 
 
@@ -16,20 +16,48 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(dest='command')
-        parser_list = subparsers.add_parser('list', cmd=self, help='list all')
+        parser_list = subparsers.add_parser('list', cmd=self, help='list all settings')
 
+        parser_get = subparsers.add_parser('get', cmd=self, help='get a specific setting')
+        parser_get.add_argument('key', help='name of the setting to get', metavar='KEY')
+
+    def write_setting_name(self, key):
+        sep = '=' * len(key)
+        self.stdout.write(self.style.HTTP_REDIRECT(key))
+        self.stdout.write(self.style.HTTP_REDIRECT(sep))
+
+    def write_setting_data(self, value):
+        data = sorted(value.items())
+        maxlength = max(len(x[0]) for x in data)
+        tmpl = "{{k: <{}}}".format(maxlength)
+        for form_key, form_value in data:
+            key = tmpl.format(k=form_key)
+            self.stdout.write("{k}: {v!r}".format(k=self.style.HTTP_INFO(key), v=form_value))
+
+    def write_setting_raw(self, value):
+        msg = "Raw dictionary"
+        sep = '=' * len(msg)
+        self.stdout.write(self.style.MIGRATE_HEADING(msg))
+        self.stdout.write(self.style.MIGRATE_HEADING(sep))
+        self.stdout.write('{!r}'.format(value))
 
     def handle(self, command, key=None, value=None, *args, **options):
+        wrapped = self.get_wrapper()
+
         if command == "list":
-            wrapped = self.get_wrapper()
             for key, value in sorted(wrapped):
-                self.stdout.write(
-                    self.style.HTTP_REDIRECT(key),
-                )
-                data = sorted(value.items())
-                maxlength = max(len(x[0]) for x in data)
-                tmpl = "{{k: <{}}}".format(maxlength)
-                for form_key, form_value in data:
-                    key = tmpl.format(k=form_key)
-                    self.stdout.write("{k}: {v!r}".format(k=self.style.HTTP_INFO(key), v=form_value))
+                self.write_setting_name(key)
+                self.write_setting_data(value)
                 self.stdout.write("\n")
+        elif command == "get":
+            if key not in wrapped:
+                raise CommandError("Invalid setting name {!r}".format(key))
+            self.write_setting_name(key)
+            value = wrapped[key]
+            self.write_setting_data(value)
+            self.stdout.write("\n")
+            self.write_setting_raw(value)
+            self.stdout.write("\n")
+        elif command == "set":
+            pass
+
