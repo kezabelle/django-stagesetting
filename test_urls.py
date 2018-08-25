35132ea677bb874a4895ce040746210308913077
Keryn Knight <keryn@kerynknight.com>
2018-08-25 14:19:08 +0100

    Fix stupid bloody urlconf changes.

--- test_urls.py
+++ test_urls.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
 from django.template.response import TemplateResponse
 from rest_framework.routers import DefaultRouter
 from stagesetting import urls as stagesetting_urls
@@ -19,9 +20,17 @@ def homepage(request):
 
 
 urlpatterns = [
-   # url(r'docs/', include('rest_framework_swagger.urls')),
-   url(r'api/', include(api.urls)),
    url(r'^$', homepage),
+   url(r'api/', include(api.urls)),
    url(r'^stagesetting/', include(stagesetting_urls)),
-   url(r'^test_admin/', include(admin.site.urls)),
 ]
+
+try:
+    extra_urlpatterns = [
+        url(r'^test_admin/', include(admin.site.urls)),
+    ]
+except ImproperlyConfigured: # bloody django ...
+    extra_urlpatterns = [
+        url(r'^test_admin/', admin.site.urls),
+    ]
+urlpatterns.extend(extra_urlpatterns)
