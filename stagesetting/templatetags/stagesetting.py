b29c99b676607df95ca1802302e76f3836f58415
Keryn Knight <keryn@kerynknight.com>
2018-09-04 21:51:34 +0100

    Fix for 1.8 assignment/as tags.

--- stagesetting/templatetags/stagesetting.py
+++ stagesetting/templatetags/stagesetting.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
+from __future__ import absolute_import, unicode_literals
+from django import VERSION as django_version
 from django.template import Library
 from stagesetting.models import RuntimeSettingWrapper, RuntimeSetting
 
 register = Library()
 
-try:
-    tag = register.assigment_tag
-except AttributeError: # Django >2.0
+if django_version[0:2] < (1, 9):
+    tag = register.assignment_tag
+else: # Django 1.9+ allows simple_tag to also be an "as tag"
     tag = register.simple_tag
 
 
